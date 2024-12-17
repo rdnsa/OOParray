8e820ed41e78fd6a4bcb8c07ab64fde3ff14028c
rdnsa <165114761+rdnsa@users.noreply.github.com>
2024-12-17 08:48:19 +0700

    Array
    Mengimplementasikan Object Oriented Programming menggunakan array

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Array {
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("ke-" + (i+1) + ": " + angka[i]);
+        }
+    }
+}
